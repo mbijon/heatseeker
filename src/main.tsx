32ca554eaa7cc18bddf0f9bd2a7133e5885ffec4
mbijonnrs <mbijon@netroadshow.com>
2025-09-15 18:07:18 -0700

    Migrate Vercel Insights from Next > React version -CC

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Heatseeker from './App';
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
