1f39a739162b84794ec316f71e5c41d51b4c03f2
mbijonnrs <mbijon@netroadshow.com>
2025-09-15 18:05:41 -0700

    Add Vercel SpeedInsights component

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Heatseeker from './App';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <Heatseeker />
-    <Analytics /> {/* Render the Vercel Analytics component */}
+    <Analytics /> {/* Vercel Analytics component */}
+    <SpeedInsights /> {/* Vercel Insights component */}
   </React.StrictMode>
 );
